0319f36f0f266152e14e6aee59bece6f447c22f8
Alex Kimeu <alexkimeu1999@gmail.com>
2022-06-11 22:57:03 +0300

    New changes to index file, styles file, js file and added new images

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+/*====== MENU SHOW Y HIDDEN ======*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*====== MENU SHOW ======*/
+/*====== VALIDATE if CONSTANT EXITS ======*/
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*====== MENU HIDDEN ======*/
+/*====== VALIDATE if CONSTANT EXITS ======*/
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*====== REMOVE MENU MOBILE ======*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //when we click on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*====== ACCORDING SKILLS ======*/
+
+/*====== QUALIFICATION TABS ======*/
+
+/*====== SERVICES MODAL ======*/
